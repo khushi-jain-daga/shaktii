db80f34fda4c3a01373685cfff5bee094b3b96c6
Khushi Jain Daga <kjain282004@gmail.com>
2026-09-18 01:01:55 +0530

    feat: add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.shaktii.pkap',
+  appName: 'SHAKTII',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
